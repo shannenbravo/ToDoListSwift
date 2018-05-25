0678c52e09250291e64a6de255a9243f10fe6b86
shannenbravo <shannenbravo@gmail.com>
2018-05-24 23:57:56 -0400

    core data added

--- ViewController.swift
+++ ViewController.swift
@@ -11,14 +11,36 @@ import CoreData
 
 class ViewController: UITableViewController {
     
-    let tasklist = [Task]()
+    var tasklist = [Task]()
     var testArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchData()
         setupNavBar()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
         tableView.tableFooterView = UIView()
+    }
+    
+    func fetchData() {
+        //create context
+        let context = CoreDataManager.shared.persistantContainer.viewContext
+        //make a fetch request
+        let fetchRequest = NSFetchRequest<Task>(entityName: "Task")
+        do {
+            let task = try context.fetch(fetchRequest)
+            
+            task.forEach({ (task) in
+                print(task.item ?? "")
+            })
+            self.tasklist = task//set ur ouside array equal to this functions comapies
+            self.tableView.reloadData() //reload the data on the table cuz we just fetched new data
+            
+        }catch let fetchErr {
+            print("Failed to fetch companies:", fetchErr)
+        }
+        
+    
     }
     
     func setupNavBar(){
@@ -38,7 +60,19 @@ class ViewController: UITableViewController {
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
         let saveAction = UIAlertAction(title: "save", style: .default) { (action) in
             guard let textField = alertController.textFields?.first, let itemToAdd = textField.text else {return}
-            self.testArray.append(itemToAdd)
+            //set contex
+            let context = CoreDataManager.shared.persistantContainer.viewContext
+            //make new todo an inset new onject
+            let newTodo = NSEntityDescription.insertNewObject(forEntityName: "Task", into: context)
+            //set the value of the newtodo
+            newTodo.setValue(itemToAdd, forKey: "item")
+            //then try to save
+            do{
+                try context.save()
+            }catch let err{
+                print("Failed to save caompany:", err)
+            }
+            self.tasklist.append(newTodo as! Task)
             self.tableView.reloadData()
         }
         alertController.addAction(cancelAction)
@@ -48,12 +82,12 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testArray.count
+        return tasklist.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        let task = testArray[indexPath.row]
+        let task = tasklist[indexPath.row].item
         cell.textLabel?.text = task
         return cell
     }
